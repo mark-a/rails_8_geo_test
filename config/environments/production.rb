401766e50ce051a9432597817de837c2f42b8bb3
Mark Duwe <ma@neopoly.de>
2025-06-13 13:01:41 +0200

    Linting...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.action_cable.allowed_request_origins = ["https://#{ENV.fetch("DEFAULT_HOST", "example.com")}"]
+  config.action_cable.allowed_request_origins = [ "https://#{ENV.fetch("DEFAULT_HOST", "example.com")}" ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
